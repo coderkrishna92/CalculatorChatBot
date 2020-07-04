6c9928dca08f0f2d0e39d9f08dec92f0cd2128dc
Pranav Krishnamurthy (TEAMS) <pkrish19@outlook.com>
2020-07-03 21:09:58 -0400

    Having the necessary changes made

--- CalculatorChatBot/Dialogs/Statistics/StandardDeviationDialog.cs
+++ CalculatorChatBot/Dialogs/Statistics/StandardDeviationDialog.cs
@@ -1,11 +1,12 @@
-﻿// <copyright file="StandardDeviationDialog.cs" company="XYZ Software LLC">
-// Copyright (c) XYZ Software LLC. All rights reserved.
+﻿// <copyright file="StandardDeviationDialog.cs" company="XYZ Software Company LLC">
+// Copyright (c) XYZ Software Company LLC. All rights reserved.
 // </copyright>
 
 namespace CalculatorChatBot.Dialogs.Statistics
 {
     using System;
     using System.Collections.Generic;
+    using System.Globalization;
     using System.Threading.Tasks;
     using CalculatorChatBot.Cards;
     using CalculatorChatBot.Models;
@@ -25,6 +26,11 @@ namespace CalculatorChatBot.Dialogs.Statistics
         /// <param name="incomingActivity">The incoming activity.</param>
         public StandardDeviationDialog(Activity incomingActivity)
         {
+            if (incomingActivity is null)
+            {
+                throw new ArgumentNullException(nameof(incomingActivity));
+            }
+
             // Parsing through the incoming message text
             string[] incomingInfo = incomingActivity.Text.Split(' ');
 
@@ -36,12 +42,30 @@ namespace CalculatorChatBot.Dialogs.Statistics
             }
         }
 
+        /// <summary>
+        /// Gets or sets the input string.
+        /// </summary>
         public string InputString { get; set; }
 
+        /// <summary>
+        /// Gets or sets the input string array.
+        /// </summary>
+#pragma warning disable CA1819 // Properties should not return arrays
         public string[] InputStringArray { get; set; }
+#pragma warning restore CA1819 // Properties should not return arrays
 
+        /// <summary>
+        /// Gets or sets the input integers.
+        /// </summary>
+#pragma warning disable CA1819 // Properties should not return arrays
         public int[] InputInts { get; set; }
+#pragma warning restore CA1819 // Properties should not return arrays
 
+        /// <summary>
+        /// This method will execute whenever this dialog is being executed at runtime.
+        /// </summary>
+        /// <param name="context">The current dialog context.</param>
+        /// <returns>A unit of execution.</returns>
         public async Task StartAsync(IDialogContext context)
         {
             if (context == null)
@@ -59,17 +83,17 @@ namespace CalculatorChatBot.Dialogs.Statistics
                 }
 
                 var mean = Convert.ToDouble(sum) / this.InputInts.Length;
-                var variance = this.CalculateVariance(mean, this.InputInts);
+                var variance = CalculateVariance(mean, this.InputInts);
                 var standardDev = Math.Sqrt((double)variance);
 
                 var successResultType = ResultTypes.StandardDeviation;
                 var results = new OperationResults()
                 {
                     Input = this.InputString,
-                    NumericalResult = standardDev.ToString(),
+                    NumericalResult = standardDev.ToString(CultureInfo.InvariantCulture),
                     OutputMsg = $"Given the list: {this.InputString}; the standard deviation = {standardDev}",
                     OperationType = operationType.GetDescription(),
-                    ResultType = successResultType.GetDescription()
+                    ResultType = successResultType.GetDescription(),
                 };
 
                 IMessageActivity successReply = context.MakeMessage();
@@ -79,11 +103,11 @@ namespace CalculatorChatBot.Dialogs.Statistics
                     new Attachment()
                     {
                         ContentType = "application/vnd.microsoft.card.adaptive",
-                        Content = JsonConvert.DeserializeObject(resultsAdaptiveCard)
-                    }
+                        Content = JsonConvert.DeserializeObject(resultsAdaptiveCard),
+                    },
                 };
 
-                await context.PostAsync(successReply);
+                await context.PostAsync(successReply).ConfigureAwait(false);
             }
             else
             {
@@ -94,7 +118,7 @@ namespace CalculatorChatBot.Dialogs.Statistics
                     NumericalResult = "0",
                     OutputMsg = "Your list may be too small to calculate the standard deviation. Please try again later",
                     OperationType = operationType.GetDescription(),
-                    ResultType = errorResType.GetDescription()
+                    ResultType = errorResType.GetDescription(),
                 };
 
                 IMessageActivity errorReply = context.MakeMessage();
@@ -104,16 +128,16 @@ namespace CalculatorChatBot.Dialogs.Statistics
                     new Attachment()
                     {
                         ContentType = "application/vnd.microsoft.card.adaptive",
-                        Content = JsonConvert.DeserializeObject(errorReplyAdaptiveCard)
-                    }
+                        Content = JsonConvert.DeserializeObject(errorReplyAdaptiveCard),
+                    },
                 };
-                await context.PostAsync(errorReply);
+                await context.PostAsync(errorReply).ConfigureAwait(false);
             }
 
             context.Done<object>(null);
         }
 
-        private decimal CalculateVariance(double mean, int[] inputInts)
+        private static decimal CalculateVariance(double mean, int[] inputInts)
         {
             double squareDiffs = 0;
             int n = inputInts.Length;
